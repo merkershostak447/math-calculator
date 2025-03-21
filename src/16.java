0cd4105f3e1baac2604e88411d941378b5572664
merkershostak447 <merkershostak447@gmail.com>
2025-03-21 08:57:28 +0000

    Add 16.java

---
+++ src/16.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class MathCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.println("Enter operator (+, -, *, /): ");
+        char op = scanner.next().charAt(0);
+        System.out.println("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        switch (op) {
+            case '+':
+                System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+                break;
+            case '-':
+                System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+                break;
+            case '*':
+                System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+                break;
+            case '/':
+                if (num2 != 0) {
+                    System.out.println(num1 + " / " + num2 + " = " + (double) num1 / num2);
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                }
+        }
+
+        scanner.close();
+    }
+}
