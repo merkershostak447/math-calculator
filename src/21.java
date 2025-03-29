789a551356b1e34547863daea9798d846cc1289b
merkershostak447 <merkershostak447@gmail.com>
2025-03-29 19:56:42 +0000

    Add 21.java

---
+++ src/21.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class MathCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter first number:");
+        double num1 = scanner.nextDouble();
+        
+        System.out.println("Enter second number:");
+        double num2 = scanner.nextDouble();
+        
+        System.out.println("Enter operation (+, -, *, /):");
+        char operation = scanner.next().charAt(0);
+        
+        switch(operation) {
+            case '+':
+                System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+                break;
+            case '-':
+                System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+                break;
+            case '*':
+                System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+                break;
+            case '/':
+                if (num2 != 0) {
+                    System.out.println(num1 + " / " + num2 + " = " + (double)(num1 / num2));
+                } else {
+                    System.out.println("Division by zero is not allowed.");
+                }
+                break;
+        }
+
+        scanner.close();
+    }
+}
