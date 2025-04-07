405ac884f6f2b9ee498ffd5550ec06aacb84a637
merkershostak447 <merkershostak447@gmail.com>
2025-04-07 03:05:47 +0000

    Add 27.java

---
+++ src/27.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+
+public class MathCalculator {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 3;
+        
+        double result = (double) num1 + (double) num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + result);
+    }
+}
