7c272d3af7ee2311ce1207c2afcec1d1826f6ed2
merkershostak447 <merkershostak447@gmail.com>
2025-05-18 13:00:47 +0000

    Add 36.java

---
+++ src/36.java
@@ -0,0 +1,9 @@
+public class MathCalculator {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+}
