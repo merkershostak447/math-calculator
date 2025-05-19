ad9056e76f254cae284b15041f7ac0b60cd4e324
merkershostak447 <merkershostak447@gmail.com>
2025-05-19 10:55:25 +0000

    Add 37.java

---
+++ src/37.java
@@ -0,0 +1,10 @@
+public class MathCalculator {
+    public static void main(String[] args) {
+        // Example calculation: 10 + 5 = 15
+        System.out.println("Result of 10 + 5 is " + (10 + 5));
+        
+        // Example calculation with floating-point numbers: 3.14 * 2 = 6.28
+        double result = 3.14 * 2;
+        System.out.println("Result of 3.14 * 2 is " + result);
+    }
+}
