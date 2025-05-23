3c506bc0ceb2ad8fc23d9504ff39e600b07f783e
merkershostak447 <merkershostak447@gmail.com>
2025-05-23 05:26:31 +0000

    Add 38.java

---
+++ src/38.java
@@ -0,0 +1,8 @@
+public class MathCalculator {
+    public static void main(String[] args) {
+        int x = 5;
+        double y = 3.14;
+
+        System.out.println("x + y: " + (x + y));
+    }
+}
